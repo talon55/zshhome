0a4acf91f21ccd1cddb0d249545b7deaf10fc66f
Eric Madar <ericm@spotify.com>
2016-11-09 14:52:39 -0500

    Docker autocompletion works!

--- docker/init.sh
+++ docker/init.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env zsh
 
-if [[ ! -s "${ZDOTDIR:-$HOME}/.zsh/docker/completion/_docker-compose"  ]]; then
-  echo "Downloading docker autocompletion file"
+if [[ ! -s "${ZDOTDIR:-$HOME}/.zsh/docker/completion"  ]]; then
   mkdir "${ZDOTDIR:-$HOME}/.zsh/docker/completion"
-  curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/zsh/_docker-compose > "${ZDOTDIR:-$HOME}/.zsh/docker/completion/_docker-compose"
+  ln -s /Applications/Docker.app/Contents/Resources/etc/docker.zsh-completion \
+    "${ZDOTDIR:-$HOME}/.zsh/docker/completion/_docker"
+  ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.zsh-completion \
+    "${ZDOTDIR:-$HOME}/.zsh/docker/completion/_docker-compose"
+  ln -s /Applications/Docker.app/Contents/Resources/etc/docker-machine.zsh-completion \
+    "${ZDOTDIR:-$HOME}/.zsh/docker/completion/_docker-machine"
 fi
 
-fpath=("${ZDOTDIR:-$HOME}/.zsh/docker/completion/" $fpath)
+fpath=($fpath "${ZDOTDIR:-$HOME}/.zsh/docker/completion/")
+
+autoload -Uz compinit && compinit -i
+
+zstyle ':completion:*:*:docker:*' option-stacking yes
+zstyle ':completion:*:*:docker-*:*' option-stacking yes
 
